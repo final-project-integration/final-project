751725d55e9dd91436cf3d37842a57f92e09c001
Farhan Chowdhury <farhanchowdhury07@gmail.com>
2025-11-06 20:29:01 -0500

    fixed package and constructor (#20)

--- storage/StorageManager.java
+++ storage/StorageManager.java
@@ -3,8 +3,7 @@ package storage;
 /**
  * The StorageManager Class handles reading and writing
  * user budget information to and from saved files.
- * 
- * @author Farhan Chowdhury
+ * * @author Farhan Chowdhury
  * @version 11/4/2025
  */
 
@@ -23,17 +22,17 @@ public class StorageManager{
 	 * @param year the year the data is being loaded for
 	 * @author Farhan Chowdhury
 	*/	
-	void loadUserData(String username, int year){
+	public void loadUserData(String username, int year){
 			
 	}
 	
 	/**
-	 *Saves the current user data to a file.
+	 * Saves the current user data to a file.
 	 * @param username the name of the user whose data is being saved
 	 * @param year the year the data is being saved for
 	 * @author Farhan Chowdhury
 	*/	
-	void saveUserData( String username, int year){
+	public void saveUserData( String username, int year){
 			
 	}
 	
@@ -43,16 +42,16 @@ public class StorageManager{
 	 * @param year the year the data is being deleted
 	 * @author Farhan Chowdhury
 	*/
-	void deleteUserData(String username, int year ){
+	public void deleteUserData(String username, int year ){
 			
 	}
 	
 	/**
-	 * Lists all the years for which the data is available.
+	 * Lists all the years for which the data is available for a user.
 	 * @param username the name of the user whose data is being accessed
 	 * @author Farhan Chowdhury
 	*/		
-	void listAvailableYears(String username){
+	public void listAvailableYears(String username){
 			
 	}
 	
@@ -63,7 +62,7 @@ public class StorageManager{
 	 * @param budgetData the updated budget information
 	 * @author Farhan Chowdhury
 	*/	
-	void updateUserBudget( String username, int year, Object budgetData){
+	public void updateUserBudget( String username, int year, Object budgetData){
 			
 	}
 	
@@ -72,13 +71,10 @@ public class StorageManager{
 	 * @param username the name of the user whose data is being accessed
 	 * @param year the year the data is being accessed
 	 * @author Farhan Chowdhury
-	 * @return The budget object with all the budget information
+	 * @return The {Budget} object with all the budget information for the specified year
 	*/	
-	Object getUserBudget(String username, int year ){
+	public Budget getUserBudget(String username, int year ){
 		return null;
 	}
 
 }
-	
-
-
