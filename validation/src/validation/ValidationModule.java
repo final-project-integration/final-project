314190368fafa7650c115ec98c22e68e31be5498
ditto-d <denicakoni@gmail.com>
2025-11-06 21:28:37 -0500

    Add validation module stubs

---
+++ validation/src/validation/ValidationModule.java
@@ -0,0 +1,14 @@
+package validation;
+
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * Validation module (the outer class) for PFM.
+ * 
+ * @author Aung Latt, Mayesha Fahmida, David Humala, Ataik
+ */
+
+public class ValidationModule {
+
+}
