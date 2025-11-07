643b4e046e27c9e798ca8858ec7579356bfcbd3e
denisa cakoni <denicakoni@gmail.com>
2025-11-06 21:50:42 -0500

    Update author comment format in DataTypeValidator

--- validation/src/validation/DataTypeValidator.java
+++ validation/src/validation/DataTypeValidator.java
@@ -6,16 +6,19 @@ import java.util.Collections;
 /**
  * Low-level, field-centric checks (emptiness, numeric, ranges, dates).
  * 
- * @author David Humala
+ * Author: David Humala
  */
-
 public class DataTypeValidator {
+
+    /**
+     * Default constructor for DataTypeValidator.
+     */
+    public DataTypeValidator() {}
 
     /**
      * @param value input string
      * @return true if non-null/non-blank after trim
      */
-
     public boolean isNonEmpty(String value) {
         return false;
     }
@@ -24,7 +27,6 @@ public class DataTypeValidator {
      * @param value numeric string
      * @return true if parses as number
      */
-
     public boolean isNumeric(String value) {
         return false;
     }
@@ -33,7 +35,6 @@ public class DataTypeValidator {
      * @param value numeric string
      * @return true if strictly greater than zero
      */
-
     public boolean isPositive(String value) {
         return false;
     }
@@ -44,7 +45,6 @@ public class DataTypeValidator {
      * @param max   inclusive max
      * @return true if within [min, max]
      */
-
     public boolean isWithinRange(String value, double min, double max) {
         return false;
     }
@@ -53,7 +53,6 @@ public class DataTypeValidator {
      * @param value date string
      * @return true if value parses to a valid date
      */
-
     public boolean isValidDate(String value) {
         return false;
     }
